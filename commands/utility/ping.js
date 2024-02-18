55edb3e30f01f0727cd2ba815380bc36a8f2be96
Antow <27694299+Antoww@users.noreply.github.com>
2024-02-18 19:25:36 +0100

    Add files via upload

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,14 +1,14 @@
 /* eslint-disable indent */
 /* eslint-disable semi */
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, Events } = require('discord.js');
 
 module.exports = {
     cooldown: 5,
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong !'),
-
     async execute(interaction) {
-        await interaction.reply('Pong !');
+        await interaction.reply(`Pong !`);
     },
+
 }
